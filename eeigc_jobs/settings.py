ff7f57fe4f730a54f0a3ab26dc9087b0000a7e83
ezraermy <ezraermy@gmail.com>
2021-08-24 16:19:45 +0300

    some test...

--- eeigc_jobs/settings.py
+++ eeigc_jobs/settings.py
@@ -134,10 +134,14 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
